c96a5a9d762e6061a040b68c29958f9e1a9f4a96
Kgosi8 <127748278+Kgosi8@users.noreply.github.com>
2025-08-05 11:42:58 +0000

    login component initiated

--- localBrandWebsite/frontend/src/app/app.routes.ts
+++ localBrandWebsite/frontend/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Login } from './components/login/login';
+import { ProfileComponent } from './components/profile-component/profile-component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'login',
+        loadComponent:()=> import('./components/login/login').then(m=>m.Login)
+    },{
+        path:'profile',
+        loadComponent:()=> import('./components/profile-component/profile-component').then(m=>m.ProfileComponent)
+    }
+
+];
--- localBrandWebsite/frontend/src/app/app.ts
+++ localBrandWebsite/frontend/src/app/app.ts
@@ -1,13 +1,18 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, computed, inject } from '@angular/core';
+import { Router, RouterLink, RouterModule, RouterOutlet } from '@angular/router';
 import { Header } from "./components/partials/header/header";
+import { CommonModule, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [Header,RouterModule,CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected title = 'frontend';
+
+  constructor(public router: Router){}
+
+
 }
